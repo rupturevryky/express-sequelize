3ae86b3e3de1ff814e83ed497fe65b18e63f9970
rupturevryky - Vladislav M <vladislav.m.vlad@gmail.com>
2023-02-08 22:22:57 +0300

    add: post, patch, delete

--- app.js
+++ app.js
@@ -15,23 +15,51 @@ app.get('/students', async (req, res) => {
     });
 });
 
+
+app.post('/students', async (req, res) => {
+    try {
+        const student = await Students.create(req.body);
+        await student.reload();
+        return res.status(201).json(student);
+    } catch (e) {
+        return res.json(e);
+    }
+});
+
+
+app.patch('/students/:id', async (req, res) => {
+    try {
+        const student = await Students.findByPk(req.params.id);
+        if (student) {
+            student.COUNTRY = req.body.COUNTRY;
+        }
+        await student.save();
+        return res.status(200).json(student);
+    } catch (e) {
+        return res.json(e);
+    }
+});
+
+
+app.delete('/students/:id', async (req, res) => {
+    try {
+        const student = await Students.findByPk(req.params.id);
+        await student.destroy();
+        return res.status(204).json();
+    } catch (e) {
+        return res.json(e);
+    }
+});
+
+
 app.listen(port, async () => {
     try {
         await Students.sync({
             alter: true,
             force: false
         });
-        const Student3 = await Students.create({ NAME: 'Alex', UNIVERSITY: 'ETH', BIRTHDAY: "2002-03-03", COUNTRY: 'Switzerland' });
+        // const Student3 = await Students.create({ NAME: 'Alex', UNIVERSITY: 'ETH', BIRTHDAY: "2002-03-03", COUNTRY: 'Switzerland' });
     } catch (error) {
         console.error(error);
     }
-});
-
-//
-
-// app.get('/', async (req, res) => {
-//     return res.send('удача')
-// });
-// app.listen(port, () => {
-//     console.log(`Сервер запущен на порту ${port}`);
-// });
+})
